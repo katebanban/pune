3a72a0db01004235c0451ede09b7214c1fc986b9
katebanban <101980471+katebanban@users.noreply.github.com>
2022-11-25 21:12:44 +0300

    adapted the "header" and the "first" sections

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@ $('.slider__body').slick({
 	infinity: true,
 	autoplay: true
 });
+
+const menuBtn = document.querySelector('.menu__btn');
+const menuList = document.querySelector('.menu__list');
+
+
+menuBtn.addEventListener('click', function () {
+	menuList.classList.toggle('menu__list--active');
+	menuBtn.classList.toggle('menu__btn--active');
+	document.querySelector('body').classList.toggle('no-scroll');
+});
